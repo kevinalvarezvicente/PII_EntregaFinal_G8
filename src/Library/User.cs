1defe726967037d182061d5696edae26eea55a2e
Carolglass <carol.glass@correo.ucu.edu.uy>
2022-06-15 22:49:20 -0300

    Agrego métodos a User y creo la interfaz IPrinter

--- src/Library/User.cs
+++ src/Library/User.cs
@@ -9,23 +9,33 @@ namespace PII_ENTREGAFINAL_G8.src.Library
     {
         private int userId;
         private string name;
-        private List<Game> gameSavedList;
+        private List<Game> gameHistory;
         private bool wantToPlay;
 
         public int UserId { get; private set; }
         public string Name { get; private set; }
-        public List<Game> GameSavedList { get; private set; }
 
         public User(int userId, string name)
         {
-            this.UserId = userId;
-            this.Name = name;
+            this.userId = userId;
+            this.name = name;
+            this.gameHistory = new List<Game>();
         }
 
         public void WantToPlay()
         {
             Lobby lobby = new Lobby();
             lobby.AddUserToList(this);
+        }
+
+        public void AddGameToGameHistory(Game game)
+        {
+            this.gameHistory.Add(game);
+        }
+
+        public List<Game> GetUserGameHistory()
+        {
+            return this.gameHistory;
         }
 
     }

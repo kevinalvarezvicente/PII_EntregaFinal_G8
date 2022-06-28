5cec94f073422d7effc5a65c8309dbb82b604c41
kevinalvarezvicente <64856845+kevinalvarezvicente@users.noreply.github.com>
2022-06-27 22:01:38 -0300

    Handlers Fixed

--- src/ChatBot_Logic/src/Handlers/RegisterUserHandler.cs
+++ src/ChatBot_Logic/src/Handlers/RegisterUserHandler.cs
@@ -9,30 +9,32 @@ namespace ChatBot_Logic.src.Handlers
     {
         public RegisterUserHandler(BaseHandler next) : base(next)
         {
-
             this.Keywords = new List<string>();
             Keywords.Add("/SerSoldado");
-
         }
 
         protected override bool InternalHandle(Telegram.Bot.Types.Message message, out string response)
         {
             ChainData chainData = ChainData.Instance;
-            string from = message.From.ToString();
+            string from = message.From.Id.ToString();
 
-            chainData.userPostionHandler[message.From.ToString()].Clear(); //Vaciamos el userPositionHandler para asi registrar el nuevo
-
-            if (this.CanHandle(message) || chainData.userPostionHandler.ContainsKey(from))
+            if (this.CanHandle(message))
             {
-                chainData.userPostionHandler[from].Add(message.Text);
+                chainData.userPostionHandler[from].Clear(); //Vaciamos el userPositionHandler para asi registrar el nuevo Handler
 
-                response = "Nuestros aliados de inteligencia 🔍 te han ahorrado el escribir tu nombre. Te hemos " +
-                    "registrado en nuestro sistema de batallas 💻 con el nombre de : 🛑 "
-                    + message.From.FirstName + " " + message.From.LastName + " 🛑\n" +
-                    "¡Tenemos un centenar de battallas ⚔️ necesitamos de tu ayuda! Unete a un escuadrón y " +
-                    "lucha contra un enemigo 💣 /batallar";
-                UsersContainer.usersContainer.Add(new User(message.From.Id, message.From.FirstName, message.From.LastName));
-                return true;
+                chainData.userPostionHandler[from].Add("/SerSoldado"); //Añadimos el nuevo handler que se esta ejecutando
+
+                if (chainData.userPostionHandler[from].Count == 1)
+                {
+                    chainData.userPostionHandler[from].Add(message.Text); // Persistimos que el Usuario esta en la primera iteración.
+                    response = "Nuestros aliados de inteligencia 🔍 te han ahorrado el escribir tu nombre. Te hemos " +
+                        "registrado en nuestro sistema de batallas 💻 con el nombre de : 🛑 "
+                        + message.From.FirstName + " " + message.From.LastName + " 🛑\n" +
+                        "¡Tenemos un centenar de battallas ⚔️ necesitamos de tu ayuda! Unete a un escuadrón y " +
+                        "lucha contra un enemigo 💣 /batallar";
+                    UsersContainer.usersContainer.Add(new User(message.From.Id, message.From.FirstName, message.From.LastName));
+                    return true;
+                }
             }
             response = string.Empty;
             return false;
